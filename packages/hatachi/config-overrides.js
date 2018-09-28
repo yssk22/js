1329b6f4d8f59b0d080e92af559deda6c5dd10d1
yssk22 <yssk22@gmail.com>
2018-09-29 00:50:21 +0900

    [hatachi] initial app package

---
+++ packages/hatachi/config-overrides.js
@@ -0,0 +1,6 @@
+/* config-overrides.js */
+const rewireYarnWorkspaces = require('react-app-rewire-yarn-workspaces');
+
+module.exports = function override(config, env) {
+  return rewireYarnWorkspaces(config, env);
+};
---
+++ packages/hatachi/src/App.js
@@ -0,0 +1,34 @@
+/* @flow */
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import { AppRoot, AppScreen, ScrollViewList } from '@yssk22/kagayaki';
+
+type Props = {};
+
+const C = () => {
+  return (
+    <ScrollViewList
+      refreshing={false}
+      data={[{ a: 1 }]}
+      childComponentFunc={(v, idx) => {
+        return (
+          <View key={idx}>
+            <Text>{v.a}</Text>
+          </View>
+        );
+      }}
+    />
+  );
+};
+
+class App extends Component<Props> {
+  render() {
+    return (
+      <AppRoot>
+        <AppScreen path="/" component={C} />
+      </AppRoot>
+    );
+  }
+}
+
+export default App;
