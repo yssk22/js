d2658c85e01453837fd856e494352336116d6b24
yssk22 <yssk22@gmail.com>
2018-09-30 00:13:49 +0900

    added type annotation

--- packages/utakata/src/RouterReducer.js
+++ packages/utakata/src/RouterReducer.js
@@ -1,3 +1,4 @@
+/* @flow */
 const REACT_NATIVE_ROUTER_FLUX_FOCUS = 'REACT_NATIVE_ROUTER_FLUX_FOCUS';
 const REACT_ROUTER_LOCATION_CHANGE = '@@router/LOCATION_CHANGE';
 
@@ -5,7 +6,10 @@ const initialState = {
   scene: {}
 };
 
-export default function reducer(state = initialState, action = {}) {
+export default function reducer(
+  state: { scene: any } = initialState,
+  action: { type?: string, scene?: any, payload?: any } = {}
+) {
   switch (action.type) {
   case REACT_NATIVE_ROUTER_FLUX_FOCUS:
     return {
