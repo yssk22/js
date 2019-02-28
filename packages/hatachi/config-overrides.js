460e277890c9d5099bf7d107c031a974035d8202
yssk22 <yssk22@gmail.com>
2019-02-28 17:18:53 +0900

    use dynamic configurations

--- packages/hatachi/config-overrides.js
+++ packages/hatachi/config-overrides.js
@@ -1,6 +1,18 @@
 /* config-overrides.js */
+const process = require('process');
 const rewireYarnWorkspaces = require('react-app-rewire-yarn-workspaces');
 
-module.exports = function override(config, env) {
-  return rewireYarnWorkspaces(config, env);
+module.exports = {
+  webpack: function(config, env) {
+    return rewireYarnWorkspaces(config, env);
+  },
+  devServer: function(configFunction) {
+    return function(proxy, allowedHost) {
+      proxy[0]['target'] = 'http://localhost:8080/';
+      if (process.env['USE_PROD_API'] == '1') {
+        proxy[0]['target'] = 'http://speedland-prod.appspot.com:8080/';
+      }
+      return configFunction(proxy);
+    };
+  }
 };
